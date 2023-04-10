aabe6b2cb5f66ad74a3a1ca732d29e1853d44da4
a2ein <111061082+a2ein@users.noreply.github.com>
2023-04-11 00:34:44 +0600

    upload to main

--- qui/sub/app2.js
+++ qui/sub/app2.js
@@ -6,13 +6,13 @@ var quiz = {
     "question": "সমবাহু ত্রিভুজের একটি বাহুকে উভয়দিকে বর্ধিত করলে যে বহিঃস্থ কোণদ্বয় উৎপন্ন হয় তাদের সমষ্টি কত?",
     "options": [
     {
-    "a": "240 ;",
-    "b": "160 ;",
-    "c": "400 ;",
-    "d": "120 ;"
+    "a": "240",
+    "b": "160",
+    "c": "400 ",
+    "d": "120 "
     }
     ],
-    "answer": "240 ;",
+    "answer": "240",
     "score": 0,
     "status": ""
     },
@@ -35,9 +35,9 @@ var quiz = {
     "question": "ABC সমকোণী ত্রিভুজ হবে যদি এর বাহুগুলোর পরিমাপ হয় মধ্যক নির্ণয়ের ক্ষেত্রে",
     "options": [
     {
-    "a": "5,12,13 একক;",
-    "b": " 6,8,10 একক;",
-    "c": "12,2,4 একক;"
+    "a": "5,12,13 একক",
+    "b": " 6,8,10 একক",
+    "c": "12,2,4 একক"
     }
     ],
     "answer": "5,12,13 একক",
@@ -46,315 +46,321 @@ var quiz = {
     },
     {
     "id": 4,
-    "question": "The external JavaScript file must contain the &lt;script&gt; tag.",
+    "question": "একটি গুণোত্তর ধারার ৩য় ও ৬ষ্ঠ পদ ৪ & ৩২ হলে ধারাটির ১m ৮টি পদের somosti কত?",
     "options": [
     {
-    "a": "True",
-    "b": "False"
+    "a":"124",
+    "b": "255", 
+    "c":"256"
     }
     ],
-    "answer": "False",
+    "answer":"255",
     "score": 0,
     "status": ""
     },
     {
     "id": 5,
-    "question": "How do you write &quot;Hello World&quot; in an alert box?",
+    "question": "4 সেমি. ও 3 সেমি ব্যাসার্ধ বিশিষ্ট দুটি বৃত্ত পরস্পরকে অন্তঃস্পর্শ করেছে। কেন্দ্রদ্বয়ের    ",
     "options": [
     {
-    "a": "alertBox(&quot;Hello World&quot;);",
-    "b": "msg(&quot;Hello World&quot;);",
-    "c": "alert(&quot;Hello World&quot;);",
-    "d": "msgBox(&quot;Hello World&quot;);",
+    "a": "circle",
+    "b": "square",
+    "c": "Tringle",
+    "d": "hex",
     }
     ],
-    "answer": "alert(&quot;Hello World&quot;);",
+    "answer": "circle",
     "score": 0,
     "status": ""
     },
     {
     "id": 6,
-    "question": "How do you create a function in JavaScript?",
+    "question": "4.3<sup>.</sup>=?",
     "options": [
     {
-    "a": "function myFunction()",
-    "b": "function:myFunction()",
-    "c": "function = myFunction()",
+    "a": "১৩/৩",
+    "b": "১২",
+    "c": "৪৫",
     }
     ],
-    "answer": "function myFunction()",
+    "answer": "১৩/৩",
     "score": 0,
     "status": ""
     },
     {
     "id": 7,
-    "question": "How do you call a function named &quot;myFunction&quot;?",
+    "question": "শতকরা বার্ষিক ৪ টাকা হার interst এ ১০০০ টাকার ৫ year এর interest কত?",
     "options": [
     {
-    "a": "call function myFunction()",
-    "b": "call myFunction()",
-    "c": "myFunction()",
+    "a": "69",
+    "b": "89",
+    "c": "200",
     }
     ],
-    "answer": "myFunction()",
+    "answer": "200",
     "score": 0,
     "status": ""
     },
     {
     "id": 8,
-    "question": "How to write an IF statement in JavaScript?",
+    "question": "(9<sup>n</sup>-1/ 3<sup>n</sup>-1)-3<sup>n</sup>=?",
     "options": [
     {
-    "a": "if i = 5 then",
-    "b": "if i == 5 then",
-    "c": "if (i == 5)",
-    "d": " if i = 5",
+    "a": "১",
+    "b": "২",
+    "c": "৪",
+    "d": "৪৫",
     }
     ],
-    "answer": "if (i == 5)",
+    "answer": "১",
     "score": 0,
     "status": ""
     },
     {
     "id": 9,
-    "question": "Which of the following is a disadvantage of using JavaScript?",
+    "question": "৬২৩৫ এর সাধারণ লগের পুর্ণক কত?",
     "options": [
     {
-    "a": "Client-side JavaScript does not allow the reading or writing of files.",
-    "b": "JavaScript can not be used for Networking applications because there is no such support available.",
-    "c": "JavaScript doesn't have any multithreading or multiprocess capabilities.",
-    "d": "All of the above."
+    "a": "১",
+    "b": "৩২",
+    "c": "২",
+    "d": "৩."
     }
     ],
-    "answer": "All of the above.",
+    "answer": "৩",
     "score": 0,
     "status": ""
     },
-    {
+    { 
     "id": 10,
-    "question": "How to write an IF statement for executing some code if &quot;i&quot; is NOT equal to 5?",
+    "question": "k এর কোন maনের জন্য 3x-y+8=0 & 6x-ky=-16 পরস্পর নিরvরশীল",
     "options": [
     {
-    "a": "if (i <> 5)",
-    "b": "if i <> 5",
-    "c": "if (i != 5)",
-    "d": "if i =! 5 then",
+    "a": "(2,8)",
+    "b": "(3,3)",
+    "c": " (8,1)",
+    "d": "(2,1)",
     }
     ],
-    "answer": "if (i != 5)",
+    "answer": "(2,1)",
     "score": 0,
     "status": ""
     },
     {
     "id": 11,
-    "question": "How does a WHILE loop start?",
+    "question": "sin0=cos0, cot0=?",
     "options": [
     {
-    "a": "while i = 1 to 10",
-    "b": "while (i &lt;= 10; i++)",
-    "c": "while (i &lt;= 10)"
+    "a": "২",
+    "b": "৪",
+    "c": "১"
     }
     ],
-    "answer": "while (i &lt;= 10)",
+    "answer": "১",
     "score": 0,
     "status": ""
     },
     {
     "id": 12,
-    "question": "How does a FOR loop start?",
+    "question": "cos0.cosec0(90<sup>০</sup>-0)=?",
     "options": [
     {
-    "a": "for (i = 0; i &lt;= 5)",
-    "b": "for (i = 0; i &lt;= 5; i++)",
-    "c": "for i = 1 to 5",
-    "d": "for (i &lt;= 5; i++)"
+    "a": "৩২",
+    "b": "২৩",
+    "c": "২",
+    "d": "১"
     }
     ],
-    "answer": "for (i = 0; i &lt;= 5; i++)",
+    "answer": "১",
     "score": 0,
     "status": ""
     },
     {
     "id": 13,
-    "question": "How can you add a comment in a JavaScript?",
+    "question": "cot0=2.4 হলে (sin0+cos0)sec0=?",
     "options": [
     {
-    "a": "//This is a comment",
-    "b": "&sbquo;This is a comment",
-    "c": "&lt;!--This is a comment--&gt;"
+    "a": "-১৭/১২",
+    "b": "১৭/১২",
+    "c": "২২/১১"
     }
     ],
-    "answer": "//This is a comment",
+    "answer": "১৭/১২",
     "score": 0,
     "status": ""
     },
     {
     "id": 14,
-    "question": "How to insert a comment that has more than one line?",
+    "question": "একটি ঘনকের আয়তন ৬৪cm<sup>৩</sup> হলে এর সmপুর্ন পৃষ্ঠের ক্ষেত্রফল কত?",
     "options": [
     {
-    "a": "/*This comment has more than one line*/",
-    "b": "//This comment has more than one line//",
-    "c": "&lt;!--This comment has more than one line--&gt;"
+    "a": "৬৫",
+    "b": "৯৬",
+    "c": "৭৭"
     }
     ],
-    "answer": "/*This comment has more than one line*/",
+    "answer": "৯৬",
     "score": 0,
     "status": ""
     },
     {
     "id": 15,
-    "question": "What is the correct way to write a JavaScript array?",
+    "question": "শ্রেনি বিন্যস্ত উপাত্তে প্রথm শ্রেনি প্রchuরক শ্রেনি হলে তার আগের শ্রেনির গনসংখ্যা কত ধরতে হয়?",
     "options": [
     {
-    "a": "var colors = (1:&quot;red&quot;, 2:&quot;green&quot;, 3:&quot;blue&quot;)",
-    "b": "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
-    "c": "var colors = 1 = (&quot;red&quot;), 2 = (&quot;green&quot;), 3 = (&quot;blue&quot;)",
-    "d": "var colors = &quot;red&quot;, &quot;green&quot;, &quot;blue&quot;"
+    "a": "৩",
+    "b": "৩২",
+    "c": "২২",
+    "d": "০"
     }
     ],
-    "answer": "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
+    "answer": "০",
     "score": 0,
     "status": ""
     },
     {
     "id": 16,
-    "question": "How do you round the number 7.25, to the nearest integer?",
+    "question": "6,4,5,11,9,8 এর mধ্যক কত?",
     "options": [
     {
-    "a": "rnd(7.25)",
-    "b": "Math.round(7.25)",
-    "c": "Math.rnd(7.25)",
-    "d": "round(7.25)"
+    "a": "৮৮",
+    "b": "৮৬",
+    "c": "৮",
+    "d": "৭"
     }
     ],
-    "answer": "Math.round(7.25)",
+    "answer": "৭",
     "score": 0,
     "status": ""
     },
     {
     "id": 17,
-    "question": "How do you find the number with the highest value of x and y?",
+    "question": "-8-3+2+7+.... ধারাটির ১৫তm পদ কত?",
     "options": [
     {
-    "a": "Math.max(x, y)",
-    "b": "Math.ceil(x, y)",
-    "c": "top(x, y)",
-    "d": "ceil(x, y)"
+    "a": "৬২",
+    "b": "৫৪৪",
+    "c": "৪২",
+    "d": "৫৪"
     }
     ],
-    "answer": "Math.max(x, y)",
+    "answer": "৬২",
     "score": 0,
     "status": ""
     },
     {
     "id": 18,
-    "question": "What is the correct JavaScript syntax for opening a new window called &quot;w2&quot;?",
+    "question": "(x – 2y, 3x + 2y ) = (1, 19 ) হলে (x, y) এর মান কোনটি?",
     "options": [
     {
-    "a": "w2 = window.new(&quot;http://www.w3schools.com&quot;);",
-    "b": "w2 = window.open(&quot;http://www.w3schools.com&quot;);"
+    "a": "(5,2)",
+    "b": "(5,23);",
+    "c":"(51,12)"
     }
     ],
-    "answer": "w2 = window.open(&quot;http://www.w3schools.com&quot;);",
+    "answer": "(5,2)",
     "score": 0,
     "status": ""
     },
     {
     "id": 19,
-    "question": "JavaScript is the same as Java.",
+    "question": " log<sub>5</sub>  (<sup>3</sup>√5.√5) এর মান কত?",
     "options": [
     {
-    "a": "true",
-    "b": "false"
+    "a": "5/6",
+    "b": "5/25",
+    "c":"3/2"
     }
     ],
-    "answer": "false",
+    "answer": "5/6",
     "score": 0,
     "status": ""
     },
     {
     "id": 20,
-    "question": "How can you detect the client&rsquo;s browser name?",
+    "question": "যে কোনো ধনাত্মক সংখ্যার বৈজ্ঞানিক রূপ ax10 <sup>n</sup>এর ক্ষেত্রে a এর সীমা নিচের কোনটি?",
     "options": [
     {
-    "a": "navigator.appName",
-    "b": "browser.name",
-    "c": "client.navName"
+    "a": "1<a< 10",
+    "b": "0<a< 1",
+    "c": "1<_a< 10"
     }
     ],
-    "answer": "navigator.appName",
+    "answer": "1<_a< 10",
     "score": 0,
     "status": ""
     },
     {
     "id": 21,
-    "question": "Which event occurs when the user clicks on an HTML element?",
-    "options": [
+    "question": "1/25<sup>-x</sup>> = 125 হলে x এর মান কত?",
+    "options":
+     [
     {
-    "a": "onchange",
-    "b": "onclick",
-    "c": "onmouseclick",
-    "d": "onmouseover"
+    "a": "3",
+    "b": "-3",
+    "c": "23",
+    "d": "-13"
     }
     ],
-    "answer": "onclick",
+
+    "answer": "3",
     "score": 0,
     "status": ""
     },
     {
     "id": 22,
-    "question": "How do you declare a JavaScript variable?",
+    "question": "4-4+4-4... ধারাটির প্রথম 2 (n + 1)",
     "options": [
     {
-    "a": "var carName;",
-    "b": "variable carName;",
-    "c": "v carName;"
+    "a": "0",
+    "b": "9",
+    "c": "8"
     }
     ],
-    "answer": "var carName;",
+    "answer": "0",
     "score": 0,
     "status": ""
     },
     {
     "id": 23,
-    "question": "Which operator is used to assign a value to a variable?",
+    "question": "f(x) = x 3 – 6x + 3 হলে f(-3) = কত?",
     "options": [
     {
-    "a": "*",
-    "b": "-",
-    "c": "=",
-    "d": "x"
+    "a": "48",
+    "b": "-6",
+    "c": "0",
+    "d": "1"
     }
     ],
-    "answer": "=",
+    "answer": "-6",
     "score": 0,
     "status": ""
     },
     {
     "id": 24,
-    "question": "What will the following code return: Boolean(10 &gt; 9)",
+    "question": "নিচের কোনটি x3 - 2x2-5x+6 এর উৎপাদক রূপ কোনটি?",
     "options": [
     {
-    "a": "NaN",
-    "b": "false",
-    "c": "true"
+    "a": "x+1",
+    "b": "x-3",
+    "c": "x+6"
     }
     ],
-    "answer": "true",
+    "answer": "x-3",
     "score": 0,
     "status": ""
     },
     {
     "id": 25,
-    "question": "Is JavaScript case-sensitive?",
+    "question": " {xEz: x2 - 9=0} এর তালিকা পদ্ধতিতে প্রকাশিত",
     "options": [
     {
-    "a": "No",
-    "b": "Yes"
+    "a": "{3,-3}",
+    "b": "{4,-3}",
+    "c":"{3,-5}"
     }
     ],
-    "answer": "Yes",
+    "answer": "{3,-3}",
     "score": 0,
     "status": ""
     }
@@ -458,3 +464,88 @@ var quiz = {
     }
     jsq.changeQuestion(-1);
     });
+
+
+
+
+    //wactg
+    var fullscreen = false;
+    var timer;
+    var running = false;
+    
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
+    
+    document.onkeydown = (e) => {
+        if (e.key === "s" || e.keyCode == 32) { ToggleTimer(); }
+        else if (e.key === "r") { ClearTimer(); }
+        else if (e.key === "f") { ToggleFullscreen(); }
+    }
+    
+    function DrawTime() {
+        document.getElementById("hour").innerText = `${hours}h`;
+        document.getElementById("minute").innerText = `${minutes}m`;
+        document.getElementById("second").innerText = `${seconds}s`;
+    }
+    
+    function ToggleFullscreen() {
+        var elem = document.documentElement;
+        if (!fullscreen) {
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.mozRequestFullScreen) { /* Firefox */
+                elem.mozRequestFullScreen();
+            } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+                elem.webkitRequestFullscreen();
+            } else if (elem.msRequestFullscreen) { /* IE/Edge */
+                elem.msRequestFullscreen();
+            }
+            document.getElementById("fullscreen-icon").innerText = "fullscreen_exit";
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) { /* Firefox */
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { /* IE/Edge */
+                document.msExitFullscreen();
+            }
+            document.getElementById("fullscreen-icon").innerText = "fullscreen";
+        }
+        fullscreen = !fullscreen;
+    }
+    
+    function ToggleTimer() {
+        if (!running) { StartTimer(); }
+        else { StopTimer(); }
+        running = !running;
+    }
+    
+    function StartTimer() {
+        timer = setInterval(function () {
+            seconds++;
+            if (seconds == 60) { seconds = 0; minutes++; }
+            if (minutes == 60) { minutes = 0; hours++; }
+    
+            DrawTime();
+    
+        }, 1000);
+        document.getElementById("start-btn").innerText = "stop";
+    }
+    
+    function StopTimer() {
+        clearInterval(timer);
+        document.getElementById("start-btn").innerText = "start";
+    }
+    
+    function ClearTimer() {
+        StopTimer();
+        hours = 0, minutes = 0, seconds = 0;
+        DrawTime();
+    }
+
+
+
+    //end
